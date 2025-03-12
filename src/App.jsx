97053bd7bda96a864561678b99263ca81c79f96d
bhaskerball52 <bhaskerball52@gmail.com>
2025-03-12 10:13:27 -0700

    Bhasker

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+      <head>
+        <title>Bhasker</title>
+      </head>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Bhasker</h1>
+      <h1>Bhasker Vasudevan</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
