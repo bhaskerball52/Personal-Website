0de178c73174e7428425e364f7db60aeb5b1bb58
bhaskerball52 <bhaskerball52@gmail.com>
2025-07-04 15:54:21 -0700

    test deploy trigger

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ function About() {
     return (
       <section className="about">
         <h2>About Me</h2>
-        <p>I’m Bhasker, a developer, hooper, scout, and future NASA beast 🚀</p>
+        <p>I’m Bhasker, a student, athlete, scout, and musician</p>
       </section>
     )
   }
